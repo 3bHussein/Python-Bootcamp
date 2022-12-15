714a6fca393e92fb5a0ed2c28e4aabc3926d25de
3bHussein <39864308+3bHussein@users.noreply.github.com>
2022-12-15 22:00:29 +0400

    Day 2

--- Day 2/2-Check Type.py
+++ Day 2/2-Check Type.py
@@ -1,2 +1,15 @@
-name=len(input("enter your name "))
-print("you name has ",name,"Characters.")
+# Data Type
+# name_len = len(input("enter your name "))
+# print("you name has ", name_len, "Characters.")
+# print(type(name_len))
+# # to change type from int to str
+# t=str(name_len)
+# print(type(t))
+
+# Code Exercise for Data Type 
+sub=(input("enter the number"))
+num1=int(sub[0])
+num2=int(sub[1])
+total=num1+num2
+
+print(num1,"+",num2,'=',total)
---
+++ Day 2/3-BMI Calculator.py
@@ -0,0 +1,4 @@
+height = float(input("enter your height\n"))
+weight = float(input("enter your weight\n"))
+BMI = weight/(height**2)
+print("your BMI is :", BMI)
