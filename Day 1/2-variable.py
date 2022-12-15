ec89b1d6b544e23dd08a08c653bd08ad425ccff6
3bHussein <39864308+3bHussein@users.noreply.github.com>
2022-12-15 21:22:48 +0400

    Finished Day 1

--- Day 1/2-variable.py
+++ Day 1/2-variable.py
@@ -1,2 +1,17 @@
 
-name =input("enter your name ")
+# name = input("enter your name ")
+# print("the type of this var is :",type(name))
+
+# print(len(name))
+
+
+a = input("a :")
+b = input("b :")
+print("a: ", a)
+print("b:", b)
+nb = a
+na = a = b
+b=nb
+
+print("a:", na)
+print("b:", nb)
---
+++ Day 1/3-Final Project.py
@@ -0,0 +1,4 @@
+print("Welcome  to the band  Name Generator")
+city=input("what's name of your city you grow up in ?\n")
+pet=input("what's your pet 's name?\n")
+print("Your band name could be",city,"",pet)
