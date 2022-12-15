6632603bec69b901b8f2896d70ace80ee579c0dd
3bHussein <39864308+3bHussein@users.noreply.github.com>
2022-12-15 19:39:05 +0400

    Update 1-print in console.py

--- Day 1/1-print in console.py
+++ Day 1/1-print in console.py
@@ -2,3 +2,6 @@
 print('"hello"')
 # to make brake between word use \n
 print("hello world \n welcome to my place")
+# TO combine two word or wto variable you need to add , between it 
+name =input("enter your name : \n")
+print("hello",name)
