34ab3f6bac2951378ff5436146d9e73e1fdb3488
3bHussein <39864308+3bHussein@users.noreply.github.com>
2022-12-15 19:47:08 +0400

    1- print in console with python

--- Day 1/1-print in console.py
+++ Day 1/1-print in console.py
@@ -1,7 +1,17 @@
-# to make "" inside print use '' first then use it 
+# to make "" inside print use '' first then use it
 print('"hello"')
 # to make brake between word use \n
 print("hello world \n welcome to my place")
-# TO combine two word or wto variable you need to add , between it 
-name =input("enter your name : \n")
-print("hello",name)
+# TO combine two word or wto variable you need to add , between it
+# name =input("enter your name : \n")
+# print("hello",name)
+
+
+# def printn(name):
+def printn():
+    name = input("enter you name :\n")
+    print('welcome ', name)
+
+
+# printn("hussein")
+printn()
