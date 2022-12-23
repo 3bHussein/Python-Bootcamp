4215447d41f3d212fab32d5a3e46384c356c8bd6
3bHussein <39864308+3bHussein@users.noreply.github.com>
2022-12-23 21:36:22 +0400

    Update 6-pizza_order.py

--- Day 3/6-pizza_order.py
+++ Day 3/6-pizza_order.py
@@ -1,5 +1,28 @@
 print("Welcome to python pizza order Deliverers")
 
-size=input("what is size do you want ?S ,M ,L")
-add_pepperoni=("Do you want add pepperoni?Y ,N")
-extra_cheese=("Do you want to add Extra Cheese?Y ,N")
+size = input("what is size do you want ?S ,M ,L")
+add_pepperoni = input("Do you want add pepperoni?Y ,N")
+extra_cheese = input("Do you want to add Extra Cheese?Y ,N")
+Bill=0
+if size == "S":
+    Bill += 7
+elif size == "M":
+    Bill += 12
+elif size == "L":
+    Bill += 15
+
+
+
+if add_pepperoni == "Y":
+    if size == "S":
+     Bill += 2
+    else:
+         Bill += 3
+if extra_cheese== "Y" :
+  Bill += 1
+
+
+else:
+    print("please Try Again ")
+
+print(f"your total bill is :{Bill}")
