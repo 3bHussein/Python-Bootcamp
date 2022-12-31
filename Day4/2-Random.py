8fb26fb2ad4c1af4e9ae1b8f57c7b8d0caa54cb5
3bHussein <39864308+3bHussein@users.noreply.github.com>
2022-12-31 15:55:00 +0400

    Update 2-Random.py

--- Day4/2-Random.py
+++ Day4/2-Random.py
@@ -1,5 +1,13 @@
 import random
 
-#Random_integer
-x =random.randint(2,6)
+# Random_integer
+x = random.randint(2, 6)
 print(x)
+
+
+r = random.random()
+print(r)
+
+love_score = random.randint(1, 100)
+print(love_score
+      )
